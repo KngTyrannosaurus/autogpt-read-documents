708f00592d0603266ad8addea15d53f03dda58c6
KngTyrannosaurus <daragh.dod@gmail.com>
2023-04-27 23:02:27 +1000

    basic PDF reading code

--- src/autogpt_read_documents/read_documents.py
+++ src/autogpt_read_documents/read_documents.py
@@ -1,1 +1,12 @@
-def 
+from PyPDF2 import PdfReader
+
+
+def read_pdf(dir):
+    # creating a pdf reader object
+    reader = PdfReader(dir)
+
+    #
+    extracted_pages = [page.extract_text() for page in reader.pages]
+
+    return '/n/n'.join(extracted_pages)
+
